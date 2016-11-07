b0c0420e5f3bbf99f9b9e5b9ac23ca455a64d8c1
wutongfei <wutongfei@kingsoft.com>
2016-11-07 15:42:32 +0800

    漏上传高斯模糊shader

---
+++ Source/FrameCapturer/Private/GaussianBlurShaders.cpp
@@ -0,0 +1,121 @@
+#include "FrameCapturerPrivatePCH.h"
+#include "GaussianBlurShaders.h"
+
+IMPLEMENT_SHADER_TYPE(, FHorizontalGaussianBlurPS, TEXT("HorizontalGaussianBlurPixelShader"), TEXT("Main"), SF_Pixel);
+IMPLEMENT_SHADER_TYPE(, FVerticalGaussianBlurPS, TEXT("VerticalGaussianBlurPixelShader"), TEXT("Main"), SF_Pixel);
+
+FHorizontalGaussianBlurPS::FHorizontalGaussianBlurPS(const ShaderMetaType::CompiledShaderInitializerType& Initializer)
+	:FGlobalShader(Initializer)
+{
+	InTexture.Bind(Initializer.ParameterMap, TEXT("InTexture"), SPF_Mandatory);
+	InTextureSampler.Bind(Initializer.ParameterMap, TEXT("InTextureSampler"));
+	InSamplerOffset.Bind(Initializer.ParameterMap, TEXT("InSamplerOffset"));
+}
+
+void FHorizontalGaussianBlurPS::SetParameters(FRHICommandList& RHICmdList, FSamplerStateRHIParamRef SamplerStateRHI, FTextureRHIParamRef TextureRHI, const float SamplerOffset)
+{
+	SetTextureParameter(RHICmdList, GetPixelShader(), InTexture, InTextureSampler, SamplerStateRHI, TextureRHI);
+	SetShaderValue(RHICmdList, GetPixelShader(), InSamplerOffset, SamplerOffset);
+}
+
+bool FHorizontalGaussianBlurPS::Serialize(FArchive& Ar)
+{
+	bool bShaderHasOutdatedParameters = FGlobalShader::Serialize(Ar);
+	Ar << InTexture;
+	Ar << InTextureSampler;
+	Ar << InSamplerOffset;
+	return bShaderHasOutdatedParameters;
+}
+
+FVerticalGaussianBlurPS::FVerticalGaussianBlurPS(const ShaderMetaType::CompiledShaderInitializerType& Initializer)
+	:FGlobalShader(Initializer)
+{
+	InTexture.Bind(Initializer.ParameterMap, TEXT("InTexture"), SPF_Mandatory);
+	InTextureSampler.Bind(Initializer.ParameterMap, TEXT("InTextureSampler"));
+	InSamplerOffset.Bind(Initializer.ParameterMap, TEXT("InSamplerOffset"));
+}
+
+void FVerticalGaussianBlurPS::SetParameters(FRHICommandList& RHICmdList, FSamplerStateRHIParamRef SamplerStateRHI, FTextureRHIParamRef TextureRHI, const float SamplerOffset)
+{
+	SetTextureParameter(RHICmdList, GetPixelShader(), InTexture, InTextureSampler, SamplerStateRHI, TextureRHI);
+	SetShaderValue(RHICmdList, GetPixelShader(), InSamplerOffset, SamplerOffset);
+}
+
+bool FVerticalGaussianBlurPS::Serialize(FArchive& Ar)
+{
+	bool bShaderHasOutdatedParameters = FGlobalShader::Serialize(Ar);
+	Ar << InTexture;
+	Ar << InTextureSampler;
+	Ar << InSamplerOffset;
+	return bShaderHasOutdatedParameters;
+}
+
+void DrawGaussianBlur(FRHICommandList& RHICmdList, const FTexture2DRHIRef& InOutTexture, const FTexture2DRHIRef& TempTarget, int32 Width, int32 Height, float Kernal)
+{
+	static const FName RendererModuleName("Renderer");
+	IRendererModule* RendererModule = &FModuleManager::GetModuleChecked<IRendererModule>(RendererModuleName);
+	
+	{
+		SetRenderTarget(RHICmdList, TempTarget, FTextureRHIRef());
+		RHICmdList.SetViewport(0, 0, 0.0f, Width, Height, 1.0f);
+
+		RHICmdList.SetBlendState(TStaticBlendState<>::GetRHI());
+		RHICmdList.SetRasterizerState(TStaticRasterizerState<>::GetRHI());
+		RHICmdList.SetDepthStencilState(TStaticDepthStencilState<false, CF_Always>::GetRHI());
+
+		const ERHIFeatureLevel::Type FeatureLevel = GMaxRHIFeatureLevel;
+
+		TShaderMap<FGlobalShaderType>* ShaderMap = GetGlobalShaderMap(FeatureLevel);
+		TShaderMapRef<FScreenVS> VertexShader(ShaderMap);
+		TShaderMapRef<FHorizontalGaussianBlurPS> PixelShader(ShaderMap);
+
+		static FGlobalBoundShaderState BoundShaderState;
+		SetGlobalBoundShaderState(RHICmdList, FeatureLevel, BoundShaderState, RendererModule->GetFilterVertexDeclaration().VertexDeclarationRHI, *VertexShader, *PixelShader);
+
+		PixelShader->SetParameters(RHICmdList, TStaticSamplerState<SF_Point>::GetRHI(), InOutTexture, Kernal / Width);
+
+		RendererModule->DrawRectangle(
+			RHICmdList,
+			0, 0,
+			Width,
+			Height,
+			0, 0,
+			1, 1,
+			FIntPoint(Width, Height),
+			FIntPoint(1, 1),
+			*VertexShader,
+			EDRF_Default);
+	}
+
+	{
+		SetRenderTarget(RHICmdList, InOutTexture, FTextureRHIRef());
+		RHICmdList.SetViewport(0, 0, 0.0f, Width, Height, 1.0f);
+
+		RHICmdList.SetBlendState(TStaticBlendState<>::GetRHI());
+		RHICmdList.SetRasterizerState(TStaticRasterizerState<>::GetRHI());
+		RHICmdList.SetDepthStencilState(TStaticDepthStencilState<false, CF_Always>::GetRHI());
+
+		const ERHIFeatureLevel::Type FeatureLevel = GMaxRHIFeatureLevel;
+
+		TShaderMap<FGlobalShaderType>* ShaderMap = GetGlobalShaderMap(FeatureLevel);
+		TShaderMapRef<FScreenVS> VertexShader(ShaderMap);
+		TShaderMapRef<FVerticalGaussianBlurPS> PixelShader(ShaderMap);
+
+		static FGlobalBoundShaderState BoundShaderState;
+		SetGlobalBoundShaderState(RHICmdList, FeatureLevel, BoundShaderState, RendererModule->GetFilterVertexDeclaration().VertexDeclarationRHI, *VertexShader, *PixelShader);
+
+		PixelShader->SetParameters(RHICmdList, TStaticSamplerState<SF_Point>::GetRHI(), TempTarget, Kernal / Height);
+
+		RendererModule->DrawRectangle(
+			RHICmdList,
+			0, 0,
+			Width,
+			Height,
+			0, 0,
+			1, 1,
+			FIntPoint(Width, Height),
+			FIntPoint(1, 1),
+			*VertexShader,
+			EDRF_Default);
+	}
+}
---
+++ Source/FrameCapturer/Private/GaussianBlurShaders.h
@@ -0,0 +1,42 @@
+#pragma once
+
+class FHorizontalGaussianBlurPS : public FGlobalShader
+{
+	DECLARE_EXPORTED_SHADER_TYPE(FHorizontalGaussianBlurPS, Global, FRAMECAPTURER_API);
+public:
+
+	static bool ShouldCache(EShaderPlatform Platform) { return true; }
+
+	FHorizontalGaussianBlurPS(const ShaderMetaType::CompiledShaderInitializerType& Initializer);
+
+	FHorizontalGaussianBlurPS() {}
+
+	void SetParameters(FRHICommandList& RHICmdList, FSamplerStateRHIParamRef SamplerStateRHI, FTextureRHIParamRef TextureRHI, const float SamplerOffset);
+
+	virtual bool Serialize(FArchive& Ar) override;
+
+private:
+	FShaderResourceParameter InTexture;
+	FShaderResourceParameter InTextureSampler;
+	FShaderParameter InSamplerOffset;
+};
+
+class FVerticalGaussianBlurPS : public FGlobalShader
+{
+	DECLARE_EXPORTED_SHADER_TYPE(FVerticalGaussianBlurPS, Global, FRAMECAPTURER_API);
+public:
+
+	static bool ShouldCache(EShaderPlatform Platform) { return true; }
+
+	FVerticalGaussianBlurPS(const ShaderMetaType::CompiledShaderInitializerType& Initializer);
+
+	FVerticalGaussianBlurPS() {}
+
+	void SetParameters(FRHICommandList& RHICmdList, FSamplerStateRHIParamRef SamplerStateRHI, FTextureRHIParamRef TextureRHI, const float SamplerOffset);
+	virtual bool Serialize(FArchive& Ar) override;
+private:
+	FShaderResourceParameter InTexture;
+	FShaderResourceParameter InTextureSampler;
+	FShaderParameter InSamplerOffset;
+};
+void DrawGaussianBlur(FRHICommandList& RHICmdList, const FTexture2DRHIRef& InOutTexture, const FTexture2DRHIRef& TempTarget, int32 Width, int32 Height, float Kernal);
